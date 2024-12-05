7762a4e2c672f9670f9abe48ea53273826b04852
Snowmonkeyboy <nedjordan@gamerstemple.com>
2024-12-05 15:46:37 -0800

    Security fix

--- main.tf
+++ main.tf
@@ -14,8 +14,7 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "default"
-{
+data "aws_vpc" "default" {
   default = true
 }
 
@@ -30,15 +29,13 @@ resource "aws_instance" "blog" {
   }
 }
 
-resource "aws_security_group" "blog"
-{
+resource "aws_security_group" "blog" {
   name= "blog"
   description = "Allow http and https in. Allow everything out"
   vpc_id = data.aws_vpc.default.id
 }
 
-resource "aws_security_group_rule"  "blog_http_in"
-{
+resource "aws_security_group_rule"  "blog_http_in" {
   type      = "ingress"
   from_port = 80
   to_port   = 80
@@ -48,8 +45,7 @@ resource "aws_security_group_rule"  "blog_http_in"
   security_group_id = aws_security_group.blog.id"
 }
 
-resource "aws_security_group_rule"  "blog_https_in"
-{
+resource "aws_security_group_rule"  "blog_https_in" {
   type      = "ingress"
   from_port = 443
   to_port   = 443
@@ -59,8 +55,7 @@ resource "aws_security_group_rule"  "blog_https_in"
   security_group_id = aws_security_group.blog.id"
 }
 
-resource "aws_security_group_rule"  "blog_everything_out"
-{
+resource "aws_security_group_rule"  "blog_everything_out" {
   type      = "egress"
   from_port = 0
   to_port   = 0
